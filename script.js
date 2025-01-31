e125a42214c7b630b4e1a24bdb02c3f03418e023
Minh <161230221+nqmpx@users.noreply.github.com>
2025-02-01 00:58:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let modalContent = document.querySelector(".pop-up");
+let openModal = document.querySelector(".rut");
+let closeModal = document.querySelector(".close-pop-up");
+let blurBg = document.querySelector(".blur-bg");
+let money = localStorage.getItem('withdrawnAmount');
+let moneyElement = document.querySelector('.money')
+let lucky = document.querySelector('.lucky-number')
+
+
+openModal.addEventListener("click", function () {
+    modalContent.classList.remove("hidden-pop-up");
+    blurBg.classList.remove("hidden-blur");
+    if (money) {
+        const luckyNumber = parseInt(lucky.value);
+        if (luckyNumber == 8 || luckyNumber == 3 || luckyNumber == 6 || luckyNumber == 30) {
+            money = Math.random() * (50000 - 1000) + 1000;
+        } else {
+            money = Math.random() * (10000 - 1000) + 1000;
+        }
+        money = Math.round(money);
+        money = money.toLocaleString();
+        localStorage.setItem('withdrawnAmount', money);
+        moneyElement.textContent = money + 'đ';
+    }
+    else{
+        moneyElement.textContent = "Rút 1 lần thui má | " + money;
+    }
+});
+
+let closeModalFunction = function () {
+  modalContent.classList.add("hidden-pop-up");
+  blurBg.classList.add("hidden-blur");
+};
+
+blurBg.addEventListener("click", closeModalFunction);
+closeModal.addEventListener("click", closeModalFunction);
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape" && !modalContent.classList.contains("hidden")) {
+    closeModalFunction();
+    }
+});
